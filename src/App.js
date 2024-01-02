a75f575e753ae9565199af14d2e7875191012b44
unknown <arunsalve318@gmail.com>
2024-01-02 20:18:49 +0530

    message

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 //import logo from './logo.svg';
 import './App.css';
-import Pagination from './components/pagination';
+import Pagination from './components/Pagination';
 
 function App() {
   return (
